c29c64e3a5654c4b32bd934ec7ec28bbed175304
alrk14567 <cdutik@gmail.com>
2024-08-26 20:55:46 +0900

    테스트

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
+import kr.nyamnyam_kr.NyamnyamKrApplication;
+import org.springframework.boot.SpringApplication;
 
 public class test {
+    public static void main(String[] args) {
+        SpringApplication.run(NyamnyamKrApplication.class, args);
+        System.out.println("Hello World");
+    }
 }
