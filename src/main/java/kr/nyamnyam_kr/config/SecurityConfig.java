30bdb7ef82add4dd93999a0bfe9297e705f281fc
alrk14567 <cdutik@gmail.com>
2024-08-31 14:10:52 +0900

    다 풀할꺼야

--- src/main/java/kr/nyamnyam_kr/config/SecurityConfig.java
+++ src/main/java/kr/nyamnyam_kr/config/SecurityConfig.java
@@ -1,7 +1,7 @@
 package kr.nyamnyam_kr.config;
 
 
-import kr.nyamnyam_kr.service.impl.UserDetailsServiceImpl;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
