28acf2108215552b461729148ae03afff80f72b2
zeongii <a01051153088@gmail.com>
2024-09-03 15:59:16 +0900

    rest

--- src/main/java/kr/nyamnyam_kr/controller/RestaurantController.java
+++ src/main/java/kr/nyamnyam_kr/controller/RestaurantController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/restaurant")
 public class RestaurantController {
