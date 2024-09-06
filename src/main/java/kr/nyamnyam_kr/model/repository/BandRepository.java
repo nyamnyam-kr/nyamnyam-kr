d5d4b16203efe4d8a6a3a89eafe3b894a9bcec88
zeongii <a01051153088@gmail.com>
2024-09-06 10:43:09 +0900

    수정

--- src/main/java/kr/nyamnyam_kr/model/repository/BandRepository.java
+++
@@ -1,7 +0,0 @@
-package kr.nyamnyam_kr.model.repository;
-
-import kr.nyamnyam_kr.model.entity.BandEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BandRepository extends JpaRepository<BandEntity, Long> {
-}
