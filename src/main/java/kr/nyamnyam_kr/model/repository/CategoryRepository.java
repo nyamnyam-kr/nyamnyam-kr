9ef6c326c933e61ffd28f259196899c5d7c844ea
zeongii <a01051153088@gmail.com>
2024-09-06 20:16:29 +0900

    이제 그만

--- src/main/java/kr/nyamnyam_kr/model/repository/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package kr.nyamnyam_kr.model.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CategoryRepository extends JpaRepository<CategoryEntity, Long> {
-
-}
