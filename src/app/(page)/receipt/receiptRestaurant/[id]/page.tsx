f6dfdbdd56f30b2a0b21ef9c53cf8617607a4d8d
zeongii <yaksanjy@naver.com>
2024-09-20 16:12:00 +0900

    no more crach

---
+++ src/app/(page)/receipt/receiptRestaurant/[id]/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+import axios from "axios";
+import React, { useEffect, useState, useRef } from 'react';
+import {useParams, useRouter} from 'next/navigation';
+
+export default function InsertReceipt() {
+    const {id} = useParams();
+
+    return (
+        <div>
+
+        </div>
+    );
+}
