e3f18a2f5cd7d0f334703f3843ab17825fd9db91
inseong6275 <david03282354@gmail.com>
2024-10-05 04:49:57 +0900

    Update Token.java

--- src/main/java/kr/nyamnyam/model/domain/Token.java
+++ src/main/java/kr/nyamnyam/model/domain/Token.java
@@ -20,11 +20,6 @@ public class Token {
     private String token; // 발급된 JWT 토큰
     private Date expirationDate; // 토큰 만료 날짜
     private Boolean isValid; // 유효 여부
-    private String username; // 사용자 이름 추가
-    private String role; // 사용자 역할 추가
-    private String nickname;
-    private Long rating;
-
 
     public Token() {
         this.isValid = true; // 기본값은 유효
