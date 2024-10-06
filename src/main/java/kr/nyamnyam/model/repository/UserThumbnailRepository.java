8509904edb7713ed0da7d01d0bf1da45076ebcfa
alrk14567 <cdutik@gmail.com>
2024-10-07 02:35:22 +0900

    user 병합 전 커밋

--- src/main/java/kr/nyamnyam/model/repository/UserThumbnailRepository.java
+++
@@ -1,8 +0,0 @@
-package kr.nyamnyam.model.repository;
-
-import kr.nyamnyam.model.entity.UserThumbnailEntity;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserThumbnailRepository extends MongoRepository<UserThumbnailEntity, Long> {
-
-}
